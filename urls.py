af5ebda732d5088642ee119b535076948402e783
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2023-07-25 11:32:44 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', views.render_grapejs, name='render_grapejs'),
     path('grapejs/<int:page_id>/', views.grapejs, name='grapejs'),
     path('save-page/<int:page_id>/', views.save_page, name='save_page'),
     path('save-exported-content/<int:page_id>/', views.save_exported_content, name='save_exported_content'),
