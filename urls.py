a006c6d6b52ad078581eff0be40d448e22f2f070
Bakhti Baymukhamedov <fanoflisataiga@gmail.com>
2023-07-25 11:15:52 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('grapejs/<int:page_id>/', views.grapejs, name='grapejs'),
+    path('save-page/<int:page_id>/', views.save_page, name='save_page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
